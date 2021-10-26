483d276611d3ae84feef5cd128db56ccdee989bd
brenex <brenex0000007@gmail.com>
2021-10-26 12:25:45 -0300

    1push

---
+++ aula-01/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Olá mundo
+        </p>
+        <a>
+          Breno Rodrigues Souza 
+        </a>
+        <br></br>
+        <a>Idade : 18</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
