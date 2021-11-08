3da734567d7458ff8de5965b73e0910543908828
brenex <brenex0000007@gmail.com>
2021-11-08 11:52:14 -0300

    commit

--- aula-08/my-app/src/App.js
+++ aula-08/my-app/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Increment from './components/Increment';
+import Toggler from './components/Toggler';
+import RemoveItemFromList from './components/RemoveItemFromList';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Increment />
+        <Toggler />
+        <RemoveItemFromList />
+      </>
+    )
+  }
 }
-
-export default App;
